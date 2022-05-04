99a6bad2f519f026ec9fb5527972f5bfc3ad9404
JsonMacK <71078248+JsonMack@users.noreply.github.com>
2022-05-04 13:13:49 -0300

    Getting started section on the main site.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use ferris_says::say;
+
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+
+    let message = String::from("Well, hello hello!");
+
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+
+    say(message.as_bytes(), width, &mut writer).unwrap();
+
 }
